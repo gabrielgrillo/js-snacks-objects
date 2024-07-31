a43e6f0762b3260f8b1adf4eee42aa5c0b7deffd
gabrielgrillo <gabrielgrillo_4@hotmail.com>
2024-07-31 20:05:02 +0200

    add snack4

---
+++ app/snack4.js
@@ -0,0 +1,30 @@
+console.log('snack 4')
+
+// snack 4:
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un’età.
+// Crea quindi un nuovo array inserendo, per ogni persona,
+//  una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
+
+let person = [
+    {
+        nome: 'Gabriel',
+        cognome: 'Grillo',
+        eta: 31,
+    },
+    {
+        nome: 'Ginevra',
+        cognome: 'Rossi',
+        eta: 27,
+    },
+    {
+        nome: 'Beppe',
+        cognome: 'Motta',
+        eta: 13,
+    },
+    {
+        nome: 'Giulia',
+        cognome: 'Mich',
+        eta: 15,
+    }
+]
